b3c969314748345fbe9c52597724c29566ea5205
Gearonix <egor.uzhanin@gmail.com>
2023-08-05 19:38:15 +0300

    chore: initialized client (Vue.js) application

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,25 @@
-module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
-  },
-};
+const { configure, presets } = require('eslint-kit')
+
+module.exports = configure({
+  root: __dirname,
+  presets: [
+    presets.node(),
+    presets.prettier({
+      singleQuote: true,
+      trailingComma: "none",
+      endOfLine: "auto"
+    }),
+    presets.typescript(),
+    presets.react(),
+  ],
+  extend: {
+    root: true,
+    ignorePatterns: ["**/*"],
+    plugins: ["prefer-arrow"],
+    // My custom config
+    extends: ["gearonix"],
+    rules: {
+
+    }
+  }
+})
