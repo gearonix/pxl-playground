0dbcd5c4099b9318416733096a813e6db927f33e
Gearonix <egor.uzhanin@gmail.com>
2023-08-06 17:49:56 +0300

    cherry-pick(deploy): fix production build

--- apps/client/src/app/router/vue-router.ts
+++ apps/client/src/app/router/vue-router.ts
@@ -1,8 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter } from 'vue-router'
 import { routes } from './routes.ts'
 
 export const router = createRouter({
   routes,
-  history: createWebHistory()
-})
+  history: createWebHashHistory()
+}) //
