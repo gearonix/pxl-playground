f1c4e70c810cd66fa9194bca79b3856174a2777b
Gearonix <egor.uzhanin@gmail.com>
2023-08-06 17:46:39 +0300

    chore(ci): fix production build

--- apps/client/src/pages/main/page.vue
+++ apps/client/src/pages/main/page.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RoutePaths } from '@/shared/config/paths.js'
+</script>
 
-<template>main-page (not ready yet)</template>
+<template>
+  main-page (not ready yet)
+  <router-link :to="RoutePaths.SIGNUP">signup</router-link>
+</template>
 
 <style scoped></style>
