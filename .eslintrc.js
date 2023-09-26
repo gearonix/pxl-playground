01b542da15091d02a6b121a1dce1b8e83b31766d
Gearonix <egor.uzhanin@gmail.com>
2023-09-26 20:31:48 +0300

    chore: improved eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,7 @@
-const { configure, presets } = require('eslint-kit')
+const grnx = require('@grnx-utils/eslint')
 
-module.exports = configure({
-  mode: 'only-errors',
+module.exports = grnx({
   root: __dirname,
-  presets: [
-    presets.node(),
-    presets.prettier({
-      singleQuote: true,
-      trailingComma: 'none'
-    }),
-    presets.typescript({
-      tsconfig: 'tsconfig.root.json',
-      root: './'
-    }),
-    presets.imports({
-      sort: {
-        newline: true
-      }
-    }),
-  ],
-  extend: {
-    root: true,
-    ignorePatterns: ['**/*'],
-    plugins: ['prefer-arrow'],
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      quotes: [2, 'single', { avoidEscape: true }],
-      '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/member-delimiter-style': [
-        'error',
-        { multiline: { delimiter: 'none' } }
-      ],
-      'import/no-unresolved': 'off',
-      '@typescript-eslint/no-unnecessary-condition': 'off',
-      'import/extensions': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'dot-notation': 'off',
-      'prefer-arrow/prefer-arrow-functions': 'error'
-    }
-  }
+  tsconfig: 'tsconfig.root.json',
+  monorepo: true
 })
